1b9131ee3997aeb22cc8a0bcd8d922a4d3cece1f
auswitch <sawit_cha@cmu.ac.th>
2022-09-23 01:30:27 +0700

    completed

--- pages/api/room.js
+++ pages/api/room.js
@@ -1,16 +1,16 @@
 import { readDB } from "../../backendLibs/dbLib";
 
-export default function roomRoute(req, res) 
+export default function roomRoute(req, res)
 {
-  const rooms = readDB();
+	const rooms = readDB();
 
-  const data = rooms.map((x) => ({
-      roomId: x.roomId,
-      roomName: x.roomName,
-  }))
+	const data = rooms.map((x) => ({
+		roomId: x.roomId,
+		roomName: x.roomName,
+	}))
 
-  return res.json({
-      ok: true,
-      rooms: data,
-  });
+	return res.json({
+		ok: true,
+		rooms: data,
+	});
 }
